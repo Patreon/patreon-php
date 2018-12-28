8187755ab4b18e10436aac8ad45049b1c6149956
root <root@thinking07.mindonthenet.com>
2018-12-28 15:32:45 +0100

    Art4 JSON lib removed

--- src/Patreon/API.php
+++ src/Patreon/API.php
@@ -1,37 +1,27 @@
 <?php
 namespace Patreon;
 
-require_once("JSONAPI/ResourceItem.php");
-require_once("JSONAPI/ResourceIdentifier.php");
-require_once("JSONAPI/Error.php");
-
 class API {
   private $access_token;
   private $manager;
 
   public function __construct($access_token) {
     $this->access_token = $access_token;
-    $factory = new \Art4\JsonApiClient\Utils\Factory([
-        'ResourceItem' => 'Patreon\JSONAPI\ResourceItem',
-        'ResourceIdentifier' => 'Patreon\JSONAPI\ResourceIdentifier',
-        'Error' => 'Patreon\JSONAPI\Error'
-    ]);
-    $this->manager = new \Art4\JsonApiClient\Utils\Manager($factory);
   }
 
-  public function fetch_user($parse = true) {
+  public function fetch_user() {
     return $this->__get_data("current_user", $parse);
   }
 
-  public function fetch_campaign_and_patrons($parse = true) {
+  public function fetch_campaign_and_patrons() {
     return $this->__get_data("current_user/campaigns?include=rewards,creator,goals,pledges", $parse);
   }
 
-  public function fetch_campaign($parse = true) {
+  public function fetch_campaign() {
     return $this->__get_data("current_user/campaigns?include=rewards,creator,goals", $parse);
   }
 
-  public function fetch_page_of_pledges($campaign_id, $page_size, $cursor = null, $parse = true) {
+  public function fetch_page_of_pledges($campaign_id, $page_size, $cursor = null) {
     $url = "campaigns/{$campaign_id}/pledges?page%5Bcount%5D={$page_size}";
     if ($cursor != null) {
       $escaped_cursor = urlencode($cursor);
@@ -40,17 +30,11 @@ class API {
     return $this->__get_data($url, $parse);
   }
 
-  private function __get_data($suffix, $parse) {
+  private function __get_data($suffix) {
       $ch = $this->__create_ch($suffix);
       $json_string = curl_exec($ch);
       $info = curl_getinfo($ch);
-      if ($parse) {
-          try {
-              return $this->manager->parse($json_string);
-          } catch ( Exception $e ) {
-              return $json_string;
-          }
-      }
+	  
       // don't try to parse a 500-class error, as it's likely not JSON
       if ($info['http_code'] >= 500) {
           return $json_string;
